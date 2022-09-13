0f404ce6b49bfae84241e30e83b2daf14cd4bf95
Jinyoung Choi <ychoijy@gmail.com>
2022-09-14 02:19:16 +0900

    Use Intentino-revealing Name
    smell : meaningless name
    
    Signed-off-by: Jinyoung Choi <ychoijy@gmail.com>

--- TennisGame1.java
+++ TennisGame1.java
@@ -2,8 +2,8 @@ package game;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1_score = 0;
+    private int player2_score = 0;
     private boolean isEnd = false;
 
     public TennisGame1() {
@@ -13,9 +13,9 @@ public class TennisGame1 implements TennisGame {
     @Override
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1_score += 1;
         else
-            m_score2 += 1;
+            player2_score += 1;
 
     }
 
@@ -23,9 +23,9 @@ public class TennisGame1 implements TennisGame {
     public String getLiteralScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1_score==player2_score)
         {
-            switch (m_score1)
+            switch (player1_score)
             {
                 case 0:
                     score = "Love-All";
@@ -42,12 +42,12 @@ public class TennisGame1 implements TennisGame {
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1_score>=4 || player2_score>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) {
+            int score_gap = player1_score-player2_score;
+            if (score_gap==1) score ="Advantage player1";
+            else if (score_gap ==-1) score ="Advantage player2";
+            else if (score_gap>=2) {
                 score = "Win for player1";
                 isEnd = true;
             }
@@ -60,8 +60,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1_score;
+                else { score+="-"; tempScore = player2_score;}
                 switch(tempScore)
                 {
                     case 0:
