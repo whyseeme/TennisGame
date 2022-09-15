c1de4b887585507a516e5ba25939d75c7b98b84a
Jinyoung Choi <ychoijy@gmail.com>
2022-09-16 02:00:07 +0900

    TennisGame1: Remove ScoreLists Method
    Initialize the List when creating an object
    
    Signed-off-by: Jinyoung Choi <ychoijy@gmail.com>

--- refactoring/TennisGame1.java
+++ refactoring/TennisGame1.java
@@ -7,10 +7,17 @@ public class TennisGame1 implements TennisGame {
 
     private Player player1;
     private Player player2;
+    private List<Score> ScoreLists;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1 = new Player(player1Name);
         this.player2 = new Player(player2Name);
+
+        ScoreLists = Arrays.asList(
+                new NormalScore(player1, player2),
+                new DrawScore(player1, player2),
+                new AdvantageScore(player1, player2),
+                new WinScore(player1, player2));
     }
 
     public void wonPoint(String playerName) {
@@ -21,20 +28,12 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getLiteralScore() {
-
-        for (Score score : ScoreLists()) {
+        for (Score score : ScoreLists) {
             if (score.isOK()) {
                 return score.toString();
             }
         }
         return "";
-    }
-
-    private List<Score> ScoreLists() {
-    	List<Score> tmpList = Arrays.asList(
-    			new AdvantageScore(player1, player2), new DrawScore(player1, player2),
-    			new NormalScore(player1, player2), new WinScore(player1, player2));
-    	return tmpList;
     }
 
     @Override
